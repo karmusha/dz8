c6aac1a0c9c13fd2b59c22d2b7eef23c987be66d
karmusha <karmusha@gmail.com>
2022-12-14 15:08:34 +0100

    kb

--- karma_bot.py
+++ karma_bot.py
@@ -1,5 +1,10 @@
+# Реализовать какой-либо функционал при помощи телеграм-бота. OR
+# Добавить систему логирования к программе из предыдущего ДЗ.
+
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Application, CommandHandler, ContextTypes, CallbackQueryHandler
+from telegram.ext import Application, CommandHandler, ContextTypes, CallbackQueryHandler, MessageHandler, filters
+import os
+from pathlib import Path
 
 api_key = '5463989628:AAHRIzmOhxMBdiWk3Gp_KE09Nu9oidOfg8U'
 
@@ -8,29 +13,28 @@ from datetime import datetime
 def sample_responses(input_text, effective_user):
     user_message = str(input_text).lower()
     
-    if user_message in ('hello', 'hi', 'привет', 'ghbdtn', 'hallo', 'хай', 'хелло', 'добрый день','1'):
+    if user_message in ('hello', 'hi', 'привет', 'ghbdtn', 'hallo', 'хай', 'хелло', 'добрый день', 'здравствуй', 'здравствуйте'):
         return f'Halo, {effective_user["first_name"]}!'
 
-    if user_message in ('время', 'время?', '2'):
+    if user_message in ('время', 'время?', 'time', 'what time?'):
         now = datetime.now()
         date_time = now.strftime('%d/%m/%y, %H:%M:%S')
         return str(date_time)
 
-    if user_message in ('ты кто', 'ты кто?', '3'):
+    if user_message in ('ты кто', 'ты кто?', 'кто ты', 'кто ты?'):
         return 'Я бот Кармы.'
     
     return 'Неизвестная команда.'
 
 print('Бот начал работу...')
 
-
 async def start_command(update: Update, context):
     keyboard = [
         [
-            InlineKeyboardButton('Поздороваться', callback_data='1'),
-            InlineKeyboardButton('Спросить время', callback_data='2'),
+            InlineKeyboardButton('Поздороваться', callback_data='привет'),
+            InlineKeyboardButton('Спросить время', callback_data='время'),
         ],
-        [InlineKeyboardButton('Узнать, кто я', callback_data = '3')],
+        [InlineKeyboardButton('Узнать, кто я', callback_data = 'ты кто')],
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
 
@@ -39,30 +43,52 @@ async def start_command(update: Update, context):
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text('Помочь вам нечем. Помогите себе сами!\nА если вы хотите узнать, что я умею, запустите команду "/start"')
 
-def handle_message(update: Update, context):
+async def handle_message(update: Update, context):
     text = str(update.message.text).lower()
     response = sample_responses(text, update.effective_user)
 
-    update.message.reply_text(response)
+    await update.message.reply_text(response)
 
-def error(update: Update, context):
+async def error(update: Update, context):
     print(f'Ваше сообщение "{update}" вызвало ошибку: {context}.')
-    update.message.reply_text('Что-то пошло не так.')
+    await update.message.reply_text('Что-то пошло не так.')
 
 async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     query = update.callback_query
     response = sample_responses(query.data, update.effective_user)
-    await query.answer()
-    # await query.edit_message_text(text=f"Вы выбрали: {query.data}")
+    # await query.answer()
     await query.edit_message_text(response)
+
+async def msg(update: Update, context):
+    file_path = Path(__file__).parent.joinpath('script.py')
+    file_to_download = await update.message.document.get_file()
+    with open(file_path, mode='bw') as f:
+        await file_to_download.download_to_memory(f)
+
+    os.system(f'python {file_path} > {str(file_path)}.out 2> {str(file_path)}.error')
+
+    text_message = None
+    with open(str(file_path) + '.out') as f:
+        text_message = f.read()
+    
+    if text_message == '':
+        with open(str(file_path) + '.error') as f:
+            text_message = f.read()
+
+    if text_message == '':
+        text_message = 'Nothing to output'
+
+    await update.message.reply_text(text_message)
 
 def main():
     dp = Application.builder().token(api_key).build()
-    dp.add_handler(CommandHandler('start', start_command))
-    dp.add_handler(CallbackQueryHandler(button))
-    dp.add_handler(CommandHandler('help', help_command))
 
-    # dp.add_handler(MessageHandler(filters.TEXT, handle_message))
+    dp.add_handler(MessageHandler(filters.Document.FileExtension('py'), msg))
+    dp.add_handler(CommandHandler('start', start_command))
+    dp.add_handler(CommandHandler('help', help_command))
+    dp.add_handler(CallbackQueryHandler(button))
+
+    dp.add_handler(MessageHandler(filters.TEXT, handle_message))
 
     dp.add_error_handler(error)
 
